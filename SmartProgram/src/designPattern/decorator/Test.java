55f68f7edf9a052f19ec15623bfe383141a6a416
enanzon <jennie.zong@ericsson.com>
2015-03-18 17:26:02 +0800

    adapter,decorator,facade

---
+++ SmartProgram/src/designPattern/decorator/Test.java
@@ -0,0 +1,12 @@
+package designPattern.decorator;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Sourceable source = new Source();
+		Sourceable dec = new Decorator(source);
+		dec.method();
+	}
+
+}
