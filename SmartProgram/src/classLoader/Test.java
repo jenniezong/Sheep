9570b9cf519a7c7ac2866eca21e9467943872da7
unknown <zongnan@BJJR-BJB1188.360buyAD.local>
2015-10-13 09:59:27 +0800

    ClassLoader class

---
+++ SmartProgram/src/classLoader/Test.java
@@ -0,0 +1,16 @@
+package classLoader;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		FileSystemClassLoader fileLoader = new FileSystemClassLoader("e:");
+		try {
+			fileLoader.findClass("Test");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
